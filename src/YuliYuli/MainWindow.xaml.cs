602ee06c036110837bf470ed4e0edf1ba5892b93
wangjinfeng <jinfeng2.wang@symbio.com>
2020-08-04 16:35:58 +0800

    修复bug

--- src/YuliYuli/MainWindow.xaml.cs
+++ src/YuliYuli/MainWindow.xaml.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Security.Policy;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Documents;
@@ -29,64 +30,76 @@ namespace YuliYuli
             this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
             Aid.Text = "";
             FilePath.Text = Environment.CurrentDirectory;
-            ProcessBar.Value = 0;
-            ProcessValue.Text = "";
             this.ResizeMode = ResizeMode.CanMinimize;
             //选择保存地址
-            FilePath.MouseDoubleClick += FilePath_MouseDoubleClick;
+            ChangeSavePath.Click += ChangeSavePath_Click;
+
             //下载
             DownFile.Click += DownFile_Click;
 
         }
-
-        private async void DownFile_Click(object sender, RoutedEventArgs e)
+        private void ChangeSavePath_Click(object sender, RoutedEventArgs e)
         {
-            ProcessBar.Value = 0;
-            ProcessValue.Text = "";
-            Result.Text = "";
-            string aid = Aid.Text.Trim();
-            if (string.IsNullOrEmpty(aid))
-            {
-                System.Windows.Forms.MessageBox.Show("请输入视频号,例如：BV1fD4y1m7TD");
+            if (!DownFile.IsEnabled)
                 return;
-            }
-            //获取视频信息
-            var video = await GetVideoInfo(aid);
-            if (string.IsNullOrEmpty(video.Title))
-            {
-                System.Windows.Forms.MessageBox.Show("视频不存在请检查视频号或者链接是否正确");
-                return;
-            }
-            var run = new Run($"{video.Title}下载中...\r\n")
-            {
-                Foreground = Brushes.Green
-            };
-            Result.Inlines.Add(run);
-            //下载视频
-            await DownLoadVideo(aid, video);
-
-            run = new Run($"{video.Title}全部下载完成.\r\n")
-            {
-                Foreground = Brushes.Green
-            };
-            Result.Inlines.Add(run);
-        }
-
-        private void FilePath_MouseDoubleClick(object sender, MouseButtonEventArgs e)
-        {
             using var dialog = new FolderBrowserDialog();
             if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
                 FilePath.Text = dialog.SelectedPath;
             }
         }
+        private async void DownFile_Click(object sender, RoutedEventArgs e)
+        {
+            Result.Inlines.Clear();
+            DownFile.IsEnabled = false;
+            string aid = Aid.Text.Trim();
+            if (string.IsNullOrEmpty(aid))
+            {
+                System.Windows.Forms.MessageBox.Show("请输入视频号或者视频链接,例如：BV1fD4y1m7TD");
+                DownFile.IsEnabled = true;
+                return;
+            }
 
-        private async Task<VideoInfo> GetVideoInfo(string aid)
+            string Url = @"^http(s)?://([\w-]+\.)+[\w-]+(/[\w- ./?%&=]*)?$";
+            if (Regex.IsMatch(aid, Url))
+            {
+                aid = aid.Replace("https://www.bilibili.com/video/", "").Split("?")[0];
+            }
+            //获取视频信息
+            var video = await GetVideoInfo(aid);
+            if (string.IsNullOrEmpty(video.Title) || video.Sections.Count <= 0)
+            {
+                System.Windows.Forms.MessageBox.Show("视频不存在请检查视频号或者链接是否正确。");
+                DownFile.IsEnabled = true;
+                return;
+            }
+
+            Result.Inlines.Add(new Run($"{video.Title}开始下载...\r\n")
+            {
+                Foreground = Brushes.Green
+            });
+            //下载视频
+            try
+            {
+                await DownLoadVideo(aid, video);
+            }
+            catch (Exception)
+            {
+                System.Windows.Forms.MessageBox.Show("下载视频出错请重试。");
+            }
+
+            Result.Inlines.Add(new Run($"{video.Title}全部下载完成.\r\n")
+            {
+                Foreground = Brushes.Green
+            });
+            DownFile.IsEnabled = true;
+        }
+        private async Task<VideoInfo> GetVideoInfo(string bvid)
         {
             var video = new VideoInfo();
             try
             {
-                var jfYu = new jfYuRequest($"https://www.bilibili.com/video/{aid}/")
+                var jfYu = new jfYuRequest($"https://api.bilibili.com/x/web-interface/view?bvid={bvid}")
                 {
                     Timeout = 30000
                 };
@@ -96,133 +109,70 @@ namespace YuliYuli
                 jfYu.RequestHeader.CacheControl = "max-age=0";
                 jfYu.RequestHeader.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36";
                 var html = await jfYu.GetHtmlAsync();
-                if (jfYu.StatusCode == System.Net.HttpStatusCode.OK && !html.Contains("视频不见了"))
+                var data = Serializer.Deserialize<dynamic>(html);
+                if (jfYu.StatusCode == System.Net.HttpStatusCode.OK && data.code == "0")
                 {
-                    //获取视频标题
-                    var doc = new HtmlAgilityPack.HtmlDocument();
-                    doc.LoadHtml(html);
-                    var title = doc.DocumentNode.SelectSingleNode("//h1[@class='video-title']").Attributes["title"].Value;
+                    //获取视频信息
+                    string title = data.data.title;
                     string reg = @"\:" + @"|du\;" + @"|\/" + @"|\\" + @"|\|" + @"|\," + @"|\*" + @"|\?" + @"|\""" + @"|\<" + @"|\>";
                     Regex r = new Regex(reg);
                     video.Title = r.Replace(title, "");
-                    //获取视频分页
-                    var videoPageReq = new jfYuRequest($"https://api.bilibili.com/x/player/pagelist?bvid={aid}&jsonp=jsonp")
-                    {
-                        Timeout = 30000
-                    };
-                    var videoPageList = Serializer.Deserialize<VideoPagedList>(await videoPageReq.GetHtmlAsync());
-                    if (videoPageList.code == 0)
-                        video.Sections = videoPageList.data;
+                    video.Sections = Serializer.Deserialize<List<VideoPagedInfo>>(data.data.pages.ToString());
+                    video.AID = data.data.aid;
+                    video.BVID = bvid;
+                    video.Mid = data.data.owner.mid;
+                    video.Name = data.data.owner.name;
                 }
             }
             catch (Exception)
             {
 
             }
-
             return video;
         }
-
         private async Task DownLoadVideo(string aid, VideoInfo video)
         {
 
             var SavePath = FilePath.Text + "\\" + video.Title + "\\";
             if (!Directory.Exists(SavePath))
                 Directory.CreateDirectory(SavePath);
+            Dictionary<string, Run> runs = new Dictionary<string, Run>();
             foreach (var item in video.Sections)
             {
-                var pTitle = item.part;
+                var fileName = $"P{item.page}{item.part}";
                 //三次重试
                 for (int j = 0; j < 4; j++)
                 {
-                    var dfreq = await DownLoadFile($"https://www.bilibili.com/video/{aid}?p={item.page}");
-                    var fileName = $"P{item.page}{pTitle}";
-                    var flag = await dfreq.GetFileAsync($"{SavePath}\\{fileName}.mp4", (q, e, t) =>
+                    var urlReq = new jfYuRequest($"https://api.bilibili.com/x/player/playurl?avid={video.AID}&cid={item.cid}&ptype=json&qn=112")
+                    {
+                        Encoding = Encoding.UTF8,
+                        Timeout = 30000
+                    };
+                    var urlReqhtml = await urlReq.GetHtmlAsync();
+                    var urlReqData = Serializer.Deserialize<dynamic>(urlReqhtml);
+                    string url = urlReqData.data.durl[0].url;
+                    var dfreq = new jfYuRequest(url);
+                    dfreq.RequestHeader.Referer = $"https://www.bilibili.com/video/{video.BVID}";
+                    runs.Add($"run{item.cid}", new Run($"{fileName},进度:0%,速度:0KB/s,剩余时间:00:00:00\r\n"));
+                    Result.Inlines.Add(runs[$"run{item.cid}"]);
+                    var flag = await dfreq.GetFileAsync($"{SavePath}\\{fileName}.flv", (q, e, t) =>
                     {
                         var pro = double.Parse(q.ToString("0.00"));
-                        var left = pro / 100 * this.Width;
-                        if (left < 20)
-                            left = 20;
-                        if (left > this.Width - 80)
-                            left = this.Width - 80;
-                        Thickness margin = new Thickness(left - 10, 63, 10, 0);
-                        ProcessPanel.Margin = margin;
-                        ProcessBar.Value = pro;
-                        ProcessValue.Text = $"{pro}%";
+                        var speed = e > 1024 ? $"{e / 1024:0.00}MB/s" : $"{e:0.00}KB/s";
+                        TimeSpan time = TimeSpan.FromSeconds((double)t);
+                        runs[$"run{item.cid}"].Text = $"{fileName},进度:{pro}%,速度:{speed},剩余时间:{time.ToString(@"hh\:mm\:ss")}\r\n";
                     });
                     if (flag)
                     {
-                        Thickness margin = new Thickness(10, 63, 10, 0);
-                        ProcessPanel.Margin = margin;
-                        ProcessBar.Value = 0;
-                        ProcessValue.Text = "";
-                        var run = new Run($"{fileName}下载完成\r\n")
-                        {
-                            Foreground = Brushes.Green
-                        };
-                        Result.Inlines.Add(run);
+                        runs[$"run{item.cid}"].Text = $"{fileName}下载完成\r\n";
                         break;
                     }
                     else
                     {
-                        var run = new Run($"{fileName}下载失败，重试第{j + 1}次\r\n")
-                        {
-                            Foreground = Brushes.Red
-                        };
-                        Result.Inlines.Add(run);
+                        runs[$"run{item.cid}"].Text = $"{fileName}下载失败，重试第{j + 1}次\r\n";
                     }
                 }
             }
-        }
-
-        private async Task<jfYuRequest> DownLoadFile(string burl)
-        {
-
-            var xbeibei = new jfYuRequest("https://www.xbeibeix.com/api/bilibili/")
-            {
-                Encoding = Encoding.UTF8,
-                Timeout = 30000
-            };
-            xbeibei.RequestHeader.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9";
-            xbeibei.RequestHeader.AcceptEncoding = "gzip, deflate, br";
-            xbeibei.RequestHeader.AcceptLanguage = "zh-CN,zh;q=0.9,en;q=0.8";
-            xbeibei.RequestHeader.CacheControl = "max-age=0";
-            xbeibei.RequestHeader.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36";
-            xbeibei.Method = jfYuRequestMethod.Get;
-            var xbeibeihtml = await xbeibei.GetHtmlAsync();
-            var xbeibeidoc = new HtmlAgilityPack.HtmlDocument();
-            xbeibeidoc.LoadHtml(xbeibeihtml);
-
-            var burlname = xbeibeidoc.DocumentNode.SelectSingleNode("//input[contains(@placeholder, '输入BiliBili视频链接地址')]").Attributes["name"].Value;
-
-            var jfYuRequest = new jfYuRequest("https://www.xbeibeix.com/api/bilibili/")
-            {
-                Encoding = Encoding.UTF8,
-                //jfYuRequest.CustomHeader.Add(":authority", "www.xbeibeix.com");
-                //jfYuRequest.CustomHeader.Add(":method", "POST");
-                //jfYuRequest.CustomHeader.Add(":path", "/api/bilibili/");
-                //jfYuRequest.CustomHeader.Add(":scheme", "https");
-                Timeout = 30000
-            };
-            jfYuRequest.RequestHeader.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9";
-            jfYuRequest.RequestHeader.AcceptEncoding = "gzip, deflate, br";
-            jfYuRequest.RequestHeader.AcceptLanguage = "zh-CN,zh;q=0.9,en;q=0.8";
-            jfYuRequest.RequestHeader.CacheControl = "max-age=0";
-            jfYuRequest.RequestHeader.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36";
-            jfYuRequest.Method = jfYuRequestMethod.Post;
-            jfYuRequest.Para.Add(burlname, burl);
-            jfYuRequest.Para.Add("zengqiang", "true");
-            var html = await jfYuRequest.GetHtmlAsync();
-            var doc = new HtmlAgilityPack.HtmlDocument();
-            doc.LoadHtml(html);
-            var title = doc.DocumentNode.SelectSingleNode("//span[contains(text(), '标题')]")?.ParentNode?.NextSibling?.NextSibling?.Attributes["value"]?.Value;
-            if (string.IsNullOrEmpty(title))
-            {
-                return null;
-            }
-            var url = doc.DocumentNode.SelectSingleNode("//a[contains(text(), 'MP4地址')]").Attributes["href"].Value;
-            return new jfYuRequest(url);
-
         }
     }
 
--- src/YuliYuli/Models/VideoInfo.cs
+++ src/YuliYuli/Models/VideoInfo.cs
@@ -7,13 +7,31 @@ namespace YuliYuli.Models
     public class VideoInfo
     {
         /// <summary>
+        /// BVID
+        /// </summary>
+        public string BVID { get; set; }
+        /// <summary>
+        /// AID
+        /// </summary>
+        public string AID { get; set; }
+        /// <summary>
         /// 标题
         /// </summary>
         public string Title { get; set; }
 
         /// <summary>
+        /// 作者编号
+        /// </summary>
+        public string Mid { get; set; }
+        
+        /// <summary>
+        /// 作者名
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
         /// 视频分P
         /// </summary>
-        public List<VideoPagedInfo> Sections { get; set; }
+        public List<VideoPagedInfo> Sections { get; set; } = new List<VideoPagedInfo>();
     }
 }
